a60335874b5e5ba1707491549bd4d04853815b7e
jh-ID <joannahong221@gmail.com>
2026-09-12 17:23:56 +0800

    jar 7

--- HomePageGUI.java
+++ HomePageGUI.java
@@ -1,6 +1,4 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class HomePageGUI {
     public static void main(String[] args) {
@@ -31,15 +29,6 @@ public class HomePageGUI {
         startButton.setBorderPainted(false);
         startButton.setContentAreaFilled(false);
         startButton.setFocusPainted(false);
-
-        startButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(
-                    frame,
-                    "START CLICKED"
-                );
-            }
-        });
 
         layeredPane.add(startButton, Integer.valueOf(1));
 

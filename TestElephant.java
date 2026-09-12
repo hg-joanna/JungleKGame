851cf694413426b170da905597af9163e948b1c5
jh-ID <joannahong221@gmail.com>
2026-09-12 17:59:43 +0800

    elephant

---
+++ TestElephant.java
@@ -0,0 +1,10 @@
+public class TestElephant {
+
+    public static void main(String[] args) {
+
+        Elephant elephant = new Elephant();
+
+        System.out.println(elephant.getName());
+        System.out.println(elephant.getStrength());
+    }
+}

17cb12f4f17d911cf61bedd3b2e8daf843a73f37
jh-ID <joannahong221@gmail.com>
2026-09-12 17:20:21 +0800

    jar 4 with start

--- HomePageGUI.java
+++ HomePageGUI.java
@@ -5,16 +5,29 @@ public class HomePageGUI {
         JFrame frame = new JFrame("Homepage Test");
         frame.setSize(1300, 900);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
 
+        // Background
         ImageIcon bg = new ImageIcon(
             HomePageGUI.class.getResource("/Resources/homepage_bg.png")
         );
 
-        System.out.println(bg.getIconWidth());
-        System.out.println(bg.getIconHeight());
+        JLabel backgroundLabel = new JLabel(bg);
+        backgroundLabel.setBounds(0, 0, 1300, 900);
+        frame.add(backgroundLabel);
 
-        JLabel label = new JLabel(bg);
-        frame.add(label);
+        // Start button
+        ImageIcon startImage = new ImageIcon(
+            HomePageGUI.class.getResource("/Resources/start_button.png")
+        );
+
+        JButton startButton = new JButton(startImage);
+        startButton.setBounds(565, 490, 191, 81);
+        startButton.setBorderPainted(false);
+        startButton.setContentAreaFilled(false);
+        startButton.setFocusPainted(false);
+
+        frame.add(startButton);
 
         frame.setVisible(true);
     }

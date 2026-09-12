10abcf4a2a824989f7049b3c3e0449d1e41f8132
jh-ID <joannahong221@gmail.com>
2026-09-12 17:14:47 +0800

    basic with image

--- HomePageGUI.java
+++ HomePageGUI.java
@@ -3,9 +3,11 @@ import javax.swing.*;
 public class HomePageGUI {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Homepage Test");
-        frame.setSize(400, 300);
+        frame.setSize(1300, 900);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JLabel label = new JLabel("HOMEPAGE WORKS");
+        ImageIcon bg = new ImageIcon("Resources/homepage_bg.png");
+        JLabel label = new JLabel(bg);
         frame.add(label);
 
         frame.setVisible(true);
